9d95bf2c45df96d4f00ef816449197a8f24be380
Test-SpringCloud <nicola_larocca@xantrix.it>
2021-05-07 10:50:44 +0200

    Aggiungere i file di progetto.

---
+++ SardiniaVillageServer/Models/Price.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SardiniaVillageServer.Models
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double PriceRoom { get; set; }
+    }
+}
