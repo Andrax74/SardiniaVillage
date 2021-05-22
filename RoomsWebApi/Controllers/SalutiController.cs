b17b9f64de18ee4534a4fe22a96f2c1e370db785
Andrax74 <info@xantrix.it>
2021-05-22 11:27:28 +0200

    Aggiunta Web Api RoomsWebApi

---
+++ RoomsWebApi/Controllers/SalutiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomsWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalutiController : ControllerBase
+    {
+        public string GetSaluti()
+        {
+            return "Saluti, sono la tua prima web api creata in C#";
+        }
+    }
+}
